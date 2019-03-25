de9b92b873f4f140427122c7a7942d04a2128f1f
Sara De Clara <saradeclara@gmail.com>
2019-03-25 21:28:16 +0000

    getMeanScore: function defined. All assigned tests pass.

--- test/week1.test.js
+++ test/week1.test.js
@@ -376,13 +376,14 @@ describe("countLinuxUsers", () => {
 
 });
 
+// getMeanScore function
 describe("getMeanScore", () => {
-  test("returns the mean score from an array of scores", () => {
+  test.only("returns the mean score from an array of scores", () => {
     expect(getMeanScore([8, 9, 7])).toBe(8);
     expect(getMeanScore([88, 86, 93])).toBe(89);
   });
 
-  test("returns the mean to 2 decimal places", () => {
+  test.only("returns the mean to 2 decimal places", () => {
     expect(getMeanScore([24, 44, 56, 11, 12, 17, 34])).toBe(28.29);
   });
 });
